86b8f4775490a06d2e4720b0808804093f6660c3
Chloe Coward <jrc0114@auburn.edu>
2022-12-02 11:55:29 -0600

    added applicative to Parsing util exports

--- src/Util/Parsing.hs
+++ src/Util/Parsing.hs
@@ -2,10 +2,12 @@
 -- Re-export the parsing module to help cut back on extra imports across the project
 module Util.Parsing
     ( module Util.Parsing
-    , module Data.Attoparsec.Text
+    , module Data.Attoparsec.Text -- Avoid re importing parsing funcs
+    , module Control.Applicative -- Useful for parsing etc
     ) where
 
 import Data.Attoparsec.Text
+import Control.Applicative
 import Data.Text ( Text, unpack )
 
 -- | Parse before a newline character
