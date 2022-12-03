838bbe35ff46b588a1a2d15869f6aa5668302545
Chloe Coward <jrc0114@auburn.edu>
2022-12-02 23:37:10 -0600

    part 1 done, going to refactor before part 2

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -5,20 +5,29 @@ import Util.Util
 import Util.Parsing ( Parser )
 import qualified Util.Parsing as P
 
+import Data.Char
+import Data.List
+
 import qualified Program.RunDay as R (runDay, Day)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
-type Input = ()
-type OutputA = ()
+type Input = [(String, String)]
+type OutputA = Int
 type OutputB = ()
 
 inputParser :: Parser Input
-inputParser = undefined
+inputParser = P.asString $ lines .> map (\s -> splitAt (length s `div` 2) s)
 
 partA :: Input -> OutputA
-partA = undefined
+partA = sum . map (sum . map prioritize . overlap)
 
 partB :: Input -> OutputB
 partB = undefined
+
+overlap (s1, s2) = nub [c | c <- s1, c `elem` s2]
+
+prioritize :: Char -> Int
+prioritize c | isUpper c = fromEnum c - 38
+             | otherwise = fromEnum c - 96
