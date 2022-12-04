19234579754e12c1652af448f729869734b9f73c
Chloe Coward <jrc0114@auburn.edu>
2022-12-03 23:11:03 -0600

    day 4 done! sad i didnt make it w a quick solve :(

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -5,20 +5,34 @@ import Util.Util
 import Util.Parsing ( Parser )
 import qualified Util.Parsing as P
 
+import Control.Arrow ((&&&))
+import Data.Ix (inRange)
+
 import qualified Program.RunDay as R (runDay, Day)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
-type Input = ()
-type OutputA = ()
-type OutputB = ()
+type Input = [((Int, Int), (Int, Int))]
+type OutputA = Int
+type OutputB = Int
 
 inputParser :: Parser Input
-inputParser = undefined
+inputParser = P.lines $ do
+    a <- P.decimal
+    b <- P.char '-' >> P.decimal
+    c <- P.char ',' >> P.decimal
+    d <- P.char '-' >> P.decimal
+    return ((a, b), (c, d))
 
 partA :: Input -> OutputA
-partA = undefined
+partA = length . filter (uncurry (||) . (uncurry within &&& uncurry (flip within)))
 
 partB :: Input -> OutputB
-partB = undefined
+partB = length . filter (uncurry (||) . (uncurry overlap &&& uncurry (flip overlap)))
+
+within :: (Int, Int) -> (Int, Int) -> Bool
+within (a, b) (c, d) = a >= c && b <= d
+
+overlap :: (Int, Int) -> (Int, Int) -> Bool
+overlap (a, b) (c, d) = inRange (a, b) c || inRange (a, b) d
