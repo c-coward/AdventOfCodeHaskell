bd16e3c4825ef8587aed2ed8cec55c3e2c367458
Chloe Coward <jrc0114@auburn.edu>
2022-12-06 09:38:36 -0600

    easy day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,24 +1,30 @@
 module Days.Day06 where
 
-import Data.Attoparsec.Text
 import Util.Util
 import Util.Parsing ( Parser )
 import qualified Util.Parsing as P
+
+import Data.List (nub)
 
 import qualified Program.RunDay as R (runDay, Day)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
-type Input = ()
-type OutputA = ()
-type OutputB = ()
+type Input = String
+type OutputA = Int
+type OutputB = Int
 
 inputParser :: Parser Input
-inputParser = undefined
+inputParser = P.getLineS
 
 partA :: Input -> OutputA
-partA = undefined
+partA = locateDistinct 4
 
 partB :: Input -> OutputB
-partB = undefined
+partB = locateDistinct 14
+
+locateDistinct :: Int -> String -> Int
+locateDistinct n xs = case splitAt n xs of
+    (y, ys) | length (nub y) == n -> n
+    _ -> 1 + locateDistinct n (tail xs)
