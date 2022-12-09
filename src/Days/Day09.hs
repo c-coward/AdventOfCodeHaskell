0b42f430e510a3368753709a9604073e52efaf47
Chloe Coward <jrc0114@auburn.edu>
2022-12-09 00:26:13 -0600

    disgusting partB. day 9 done

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -1,24 +1,48 @@
 module Days.Day09 where
 
-import Data.Attoparsec.Text
 import Util.Util
 import Util.Parsing ( Parser )
 import qualified Util.Parsing as P
+
+import Data.Char ( isSpace )
+import Data.List ( nub )
+import Control.Monad
 
 import qualified Program.RunDay as R (runDay, Day)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
-type Input = ()
-type OutputA = ()
-type OutputB = ()
+type Input = [Pair Int]
+type OutputA = Int
+type OutputB = Int
 
 inputParser :: Parser Input
-inputParser = undefined
+inputParser = concat <$> P.lines parseMove
 
 partA :: Input -> OutputA
-partA = undefined
+partA = length . nub . scanl follow (P 0 0) . scanl (+) (P 0 0)
 
 partB :: Input -> OutputB
-partB = undefined
+partB = scanl (+) (P 0 0) .> scanl follow (P 0 0) .> scanl follow (P 0 0) .> scanl follow (P 0 0) .> tail
+     .> scanl follow (P 0 0) .> scanl follow (P 0 0) .> scanl follow (P 0 0) .> tail
+      .> scanl follow (P 0 0) .> scanl follow (P 0 0) .> scanl follow (P 0 0) .> tail
+      .> nub .> length
+
+-- Basic moves
+r = P 0 1
+l = P 0 (-1)
+u = P 1 0
+d = P (-1) 0
+
+follow :: Pair Int -> Pair Int -> Pair Int
+follow t h = case h - t of
+    p@P{..} | (x == 0 && abs y > 1) || (y == 0 && abs x > 1) -> t + signum p
+            | x /= 0 && y /= 0 && (abs x > 1 || abs y > 1) -> t + signum p
+            | otherwise -> t
+
+parseMove :: Parser [Pair Int]
+parseMove = do
+    c <- P.takeWhile (not . isSpace)
+    n <- P.skipSpace >> P.decimal
+    return . replicate n $ case c of {"R" -> r; "L" -> l; "U" -> u; "D" -> d}
--- src/Util/Util.hs
+++ src/Util/Util.hs
@@ -17,3 +17,19 @@ infixr 1 .>, |>
 
 (&&&+) :: (a -> b -> c) -> (a -> b -> d) -> a -> b -> (c, d)
 f &&&+ g = \a b -> (f a b, g a b)
+
+rep f a = f a a
+
+data Pair a = P {y :: a, x :: a} deriving (Show, Eq)
+instance Functor Pair where
+    fmap f (P a b) = P (f a) (f b)
+instance Applicative Pair where
+    pure = rep P
+    P f g <*> P a b = P (f a) (g b)
+instance Num a => Num (Pair a) where
+    a + b = (+) <$> a <*> b
+    a - b = (-) <$> a <*> b
+    a * b = (*) <$> a <*> b
+    abs = (abs <$>)
+    signum = (signum <$>)
+    fromInteger = pure . fromInteger
