4ecc94ceaef4cebc4219a46c30525d6411bce969
Chloe Coward <jrc0114@auburn.edu>
2022-12-19 18:25:10 -0800

    day 18 part 1

--- src/Days/Day18.hs
+++ src/Days/Day18.hs
@@ -1,24 +1,37 @@
 module Days.Day18 where
 
-import Data.Attoparsec.Text
 import Util.Util
 import Util.Parsing ( Parser )
 import qualified Util.Parsing as P
+
+import qualified Data.Set as S
+import qualified Data.Map as M
+import Control.Monad.State
 
 import qualified Program.RunDay as R (runDay, Day)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
-type Input = ()
-type OutputA = ()
+type Input = [(Int, Int, Int)]
+type OutputA = Int
 type OutputB = ()
 
 inputParser :: Parser Input
-inputParser = undefined
+inputParser = P.lines parseLine
 
 partA :: Input -> OutputA
-partA = undefined
+partA = (map =<< exposed . S.fromList) .> concat .> length
 
 partB :: Input -> OutputB
 partB = undefined
+
+allPoints3D :: [(Int, Int, Int)]
+allPoints3D = [(x, y, z) | d <- [0..]
+    , x <- [0..d], y <- [0..d], let z = d - x - y, z >= 0]
+
+exposed :: S.Set (Int, Int, Int) -> (Int, Int, Int) -> [(Int, Int, Int)]
+exposed s (x,y,z) = filter (`S.notMember` s)
+    $ concat [[(x+d,y,z), (x,y+d,z), (x,y,z+d)] | d <- [-1,1]]
+
+parseLine = (,,) <$> P.decimal <*> (P.anyChar >> P.decimal) <*> (P.anyChar >> P.decimal)
